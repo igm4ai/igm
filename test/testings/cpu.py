28db5ee74a3457a22a5d0d7a89680fe18180083b
HansBug <hansbug@buaa.edu.cn>
2022-08-25 15:05:23 +0800

    dev(hansbug): add cpu test info

---
+++ test/testings/cpu.py
@@ -0,0 +1,18 @@
+CPU_INFO_1 = {
+    'python_version': '3.7.6.final.0 (64 bit)', 'cpuinfo_version': [8, 0, 0],
+    'cpuinfo_version_string': '8.0.0', 'arch': 'X86_64', 'bits': 64, 'count': 6, 'arch_string_raw': 'x86_64',
+    'vendor_id_raw': 'GenuineIntel', 'brand_raw': 'Intel(R) Core(TM) i7-10750H CPU @ 2.60GHz',
+    'hz_advertised_friendly': '2.6000 GHz', 'hz_actual_friendly': '2.5920 GHz',
+    'hz_advertised': [2600000000, 0], 'hz_actual': [2592002000, 0], 'stepping': 2, 'model': 165, 'family': 6,
+    'flags': ['3dnowprefetch', 'abm', 'aes', 'apic', 'arch_capabilities', 'avx', 'avx2', 'clflush',
+              'clflushopt', 'cmov', 'constant_tsc', 'cpuid', 'cx16', 'cx8', 'de', 'flush_l1d', 'fpu',
+              'fsgsbase', 'fxsr', 'ht', 'hypervisor', 'invpcid', 'invpcid_single', 'lahf_lm', 'lm', 'mca',
+              'mce', 'md_clear', 'mmx', 'movbe', 'msr', 'mtrr', 'nonstop_tsc', 'nopl', 'nx', 'osxsave', 'pae',
+              'pat', 'pcid', 'pclmulqdq', 'pge', 'pni', 'popcnt', 'pse', 'pse36', 'rdrand', 'rdrnd', 'rdseed',
+              'rdtscp', 'rep_good', 'sep', 'sse', 'sse2', 'sse4_1', 'sse4_2', 'ssse3', 'syscall', 'tsc',
+              'tsc_known_freq', 'vme', 'x2apic', 'xsave', 'xtopology'], 'l3_cache_size': 12582912,
+    'l2_cache_size': 65536, 'l2_cache_line_size': 256, 'l2_cache_associativity': 6,
+    'cpus': [{'percentage': 8.2, 'frequency': 2592.002}, {'percentage': 5.5, 'frequency': 2592.002},
+             {'percentage': 1.8, 'frequency': 2592.002}, {'percentage': 3.7, 'frequency': 2592.002},
+             {'percentage': 3.6, 'frequency': 2592.002}, {'percentage': 97.3, 'frequency': 2592.002}]
+}
