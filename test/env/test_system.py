5226ca8ccc9d47cebb0cac21ca769be68765a17a
HansBug <hansbug@buaa.edu.cn>
2022-08-26 20:47:46 +0800

    dev(hansbug): add test for sys.internet

--- test/env/test_system.py
+++ test/env/test_system.py
@@ -10,6 +10,7 @@ from igm.env import sys
 from ..testings import ONE_GPU_1_DATA, CPU_INFO_1, CPU_INFO_100, MEMORY_INFO_100
 
 
+# noinspection DuplicatedCode
 @pytest.mark.unittest
 class TestEnvSystem:
     @patch('igm.env.system.get_nvidia_info', MagicMock(return_value=ONE_GPU_1_DATA))
@@ -100,3 +101,75 @@ class TestEnvSystem:
     @skipUnless(not psutil.swap_memory().total, 'no swap memory required')
     def test_no_swap_actual(self):
         assert not sys.swap
+
+    @pytest.mark.flaky(reruns=3, reruns_delay=2)
+    @skipUnless(not os.getenv('NO_INTERNET'), 'sys.internet required')
+    @skipUnless(not os.getenv('NO_GFW'), 'gfw required')
+    def test_internet_actual_has_internet_in_gfw(self):
+        assert sys.internet
+        assert sys.internet.has_internet
+        assert sys.internet.has_gfw
+        assert repr(sys.internet) == '<Internet ok, gfw: yes>'
+
+        assert sys.internet.baidu
+        assert sys.internet.gitee
+        assert sys.internet.github
+        assert not sys.internet.google
+
+        assert sys.internet.baidu
+        assert sys.internet.baidu.ok
+        assert 0.0 <= sys.internet.baidu.ttl <= 1.0
+        assert sys.internet.baidu.address == 'baidu.com'
+        assert sys.internet.baidu.port == 80
+        assert repr(sys.internet.baidu).startswith('<BaiduConnect baidu.com:80, success, ttl:') and \
+               repr(sys.internet.baidu).endswith('ms>')
+
+        assert not sys.internet.google
+        assert not sys.internet.google.ok
+        assert sys.internet.google.ttl is None
+        assert sys.internet.google.address == 'google.com'
+        assert sys.internet.google.port == 80
+        assert repr(sys.internet.google) == '<GoogleConnect google.com:80, fail>'
+
+        assert not sys.internet('twitter.com')
+        assert not sys.internet('twitter.com')
+        assert sys.internet('twitter.com').ttl is None
+        assert sys.internet('twitter.com').address == 'twitter.com'
+        assert sys.internet('twitter.com').port == 80
+        assert repr(sys.internet('twitter.com')) == '<ConnectStatus twitter.com:80, fail>'
+
+    @pytest.mark.flaky(reruns=3, reruns_delay=2)
+    @skipUnless(not os.getenv('NO_INTERNET'), 'internet required')
+    @skipUnless(os.getenv('NO_GFW'), 'no gfw required')
+    def test_internet_actual_has_internet_out_of_gfw(self):
+        assert sys.internet
+        assert sys.internet.has_internet
+        assert not sys.internet.has_gfw
+        assert repr(sys.internet) == '<Internet ok, gfw: no>'
+
+        assert sys.internet.baidu
+        assert sys.internet.gitee
+        assert sys.internet.github
+        assert sys.internet.google
+
+        assert sys.internet.baidu
+        assert sys.internet.baidu.ok
+        assert 0.0 <= sys.internet.baidu.ttl <= 1.0
+        assert sys.internet.baidu.address == 'baidu.com'
+        assert sys.internet.baidu.port == 80
+        assert repr(sys.internet.baidu).startswith('<BaiduConnect baidu.com:80, success, ttl:') and \
+               repr(sys.internet.baidu).endswith('ms>')
+
+        assert sys.internet.google
+        assert sys.internet.google.ok
+        assert 0.0 <= sys.internet.google.ttl <= 1.0
+        assert sys.internet.google.address == 'google.com'
+        assert sys.internet.google.port == 80
+        assert repr(sys.internet.google).startswith('<GoogleConnect google.com:80, success, ttl:') and \
+               repr(sys.internet.google).endswith('ms>')
+
+    @skipUnless(os.getenv('NO_INTERNET'), 'no internet required')
+    def test_internet_actual_no_network(self):
+        assert not sys.internet
+        assert not sys.internet.has_internet
+        assert repr(sys.internet) == '<Internet unavailable>'
